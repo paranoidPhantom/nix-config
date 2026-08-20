1b7d756e62b6849c6c24c52702b81fa2de9fb9c6
Andrei Hudalla <andrei@hudalla.dev>
2026-08-19 20:00:38 -0500

    feat(darwin): get system to usable state

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,1 +1,33 @@
-{ ... }: {}
+{ pkgs, ... }: {
+  system.defaults = {
+    # Global OS behaviors
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleIconAppearanceTheme = "ClearAutomatic";
+    };
+
+    # Dock preferences
+    dock = {
+      autohide = true;
+      orientation = "bottom";
+      show-recents = false;
+      minimize-to-application = false;
+    };
+
+    # Finder preferences
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+    };
+  };
+
+  # Homebrew preferences
+  homebrew = {
+    enable = true;
+    enableZshIntegration = true;
+    casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" ];
+  };
+
+  # Sudo with Touch ID
+  security.pam.services.sudo_local.touchIdAuth = true;
+}
