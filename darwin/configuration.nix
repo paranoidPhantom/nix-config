b4cfcc59ff08a17a442ae9ab29a7d00907449457
Andrei Hudalla <andrei@hudalla.dev>
2026-08-21 22:07:16 -0500

    feat(darwin): configure system

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }: {
+  # Power
+  power.sleep.display = 60;
+
   system.defaults = {
     # Global OS behaviors
     NSGlobalDomain = {
       AppleInterfaceStyle = "Dark";
-      AppleIconAppearanceTheme = "ClearAutomatic";
+      AppleIconAppearanceTheme = "RegularDark";
+      AppleMeasurementUnits = "Centimeters";
+      AppleMetricUnits = 1;
+      AppleTemperatureUnit = "Celsius";
+    };
+
+    # Window management
+    WindowManager = {
+      StandardHideWidgets = true;
+      StandardHideDesktopIcons = true;
+      EnableStandardClickToShowDesktop = false;
+      EnableTilingByEdgeDrag = false;
     };
 
     # Dock preferences
@@ -12,6 +26,7 @@
       orientation = "bottom";
       show-recents = false;
       minimize-to-application = false;
+      tilesize = 10;
     };
 
     # Finder preferences
@@ -19,13 +34,17 @@
       AppleShowAllExtensions = true;
       ShowPathbar = true;
     };
+
+    controlcenter.BatteryShowPercentage = false;
+    menuExtraClock.IsAnalog = true;
   };
 
   # Homebrew preferences
   homebrew = {
     enable = true;
     enableZshIntegration = true;
-    casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" ];
+    brews = (import ../software.nix { pkgs = pkgs; }).formulae;
+    casks = (import ../software.nix { pkgs = pkgs; }).casks;
   };
 
   # Sudo with Touch ID
