3daa4d6059fb201432e1104749a818dc4939ffc7
Andrei Hudalla <andrei@hudalla.dev>
2026-09-05 14:39:49 +0300

    software(add): ktalk, zed editor

--- software.nix
+++ software.nix
@@ -18,5 +18,5 @@
     vtsls
   ];
   formulae = [ "zsh-autocomplete" ];
-  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" ];
+  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" "kontur-talk" "zed" ];
 }
