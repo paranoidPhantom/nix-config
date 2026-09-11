eb6449b1432a72a65e31eb92a7234910da5f769d
Andrei Hudalla <andrei@hudalla.dev>
2026-09-11 12:15:19 +0300

    software(add): pnpm, obs

--- software.nix
+++ software.nix
@@ -17,6 +17,6 @@
     nodejs_26
     vtsls
   ];
-  formulae = [ "zsh-autocomplete" "opencode" ];
-  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" "kontur-talk" "zed" ];
+  formulae = [ "zsh-autocomplete" "opencode" "pnpm" ];
+  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" "kontur-talk" "zed" "obs" ];
 }
