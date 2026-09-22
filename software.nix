9199c040ae5c8dd6e51bc3c5b6c4a708b0d47ffb
Andrei Hudalla <andrei@hudalla.dev>
2026-09-22 11:44:32 +0300

    software(add): zoom

--- software.nix
+++ software.nix
@@ -17,5 +17,5 @@
     bacon
   ];
   formulae = [ "zsh-autocomplete" "opencode" "pnpm" ];
-  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" "kontur-talk" "zed" "obs" "handbrake" ];
+  casks = [ "ghostty" "secretive" "bitwarden" "zen" "Roblox" "rectangle" "stats" "telegram" "vlc" "transmission" "happ" "cleanshot" "obsidian" "gimp" "kontur-talk" "zed" "obs" "handbrake" "zoom" ];
 }
